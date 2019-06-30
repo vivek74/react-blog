0382be1b1aa7fb4f8c1e786548c1e165e50be402
vivek74 <vivekkumar74@yahoo.in>
2019-06-30 10:39:23 +0530

    uploaded

---
+++ client/build/precache-manifest.fcb35da642b728dfce5aac2cc1b2a758.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "648c571fbde35788480ae4cc8dd2ac3c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a3ef32d4371b52ac604",
+    "url": "/static/css/2.c1457fc7.chunk.css"
+  },
+  {
+    "revision": "86f40b7917a52a402758",
+    "url": "/static/css/main.36738390.chunk.css"
+  },
+  {
+    "revision": "3a3ef32d4371b52ac604",
+    "url": "/static/js/2.0348493f.chunk.js"
+  },
+  {
+    "revision": "86f40b7917a52a402758",
+    "url": "/static/js/main.c4a736d0.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  }
+]);
